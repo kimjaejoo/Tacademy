7089efccdb98bc12b4c9ab03a5816451761dfc1e
kimjaejoo <kjj0710@naver.com>
2016-10-31 17:51:38 +0900

    04 . commit : 앞전에는 어디서 commit을 해야 할지 몰라서 그냥 했음 이제는 이 다음 commit부터는 내용이 수정이 완료되면 commit하겠음
    json, 여러가지 환경설정 , servlet mysql을 사용함

---
+++ Hello/src/com/model/ReqBody.java
@@ -0,0 +1,34 @@
+package com.model;
+
+import java.util.ArrayList;
+
+public class ReqBody {
+
+	int head;
+	
+	ReqLogin body;
+
+	public int getHead() {
+		return head;
+	}
+
+	public void setHead(int head) {
+		this.head = head;
+	}
+
+	public ReqLogin getBody() {
+		return body;
+	}
+
+	public void setBody(ReqLogin body) {
+		this.body = body;
+	}
+	
+	
+	
+	
+	
+
+	
+	
+}
---
+++ Hello/src/com/model/ReqLogin.java
@@ -0,0 +1,25 @@
+package com.model;
+
+public class ReqLogin {
+	String uid, upw;
+
+	
+	//getter, setter
+	public String getUid() {
+		return uid;
+	}
+
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+
+	public String getUpw() {
+		return upw;
+	}
+
+	public void setUpw(String upw) {
+		this.upw = upw;
+	}
+	
+	
+}
